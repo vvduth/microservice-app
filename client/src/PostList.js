98b80eb8487eae21f45f1fc170e0f719a63ffe53
vvduth <ducthai060501@gmail.com>
2024-12-22 12:17:59 +0200

    created event bus

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 const PostList = () => {
   const [posts, setPosts] = useState({});
 
@@ -23,6 +25,8 @@ const PostList = () => {
         <div className="card-body">
           <h3>{post.title}</h3>
         </div>
+        <CommentCreate postId={post.id} />
+        <CommentList postId={post.id} />
       </div>
     );
   });
---
+++ event-bus/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const app = express();
+const port = 4005;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.post("/events", (req, res) => {
+  const event = req.body;
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  res.send({ status: "OK" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
