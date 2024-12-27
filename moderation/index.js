aecd48b468b298bc49c1a6dd50b0df76ba0b245e
vvduth <ducthai060501@gmail.com>
2024-12-27 17:19:44 +0200

    created deployment and cluster service  for moderation, query and comments

--- moderation/index.js
+++ moderation/index.js
@@ -14,7 +14,7 @@ app.post("/events", async (req, res) => {
     console.log("status", status);
 
     await axios
-      .post("http://localhost:4005/events", {
+      .post("http://event-bus-srv:4005/events", {
         type: "CommentModerated",
         data: {
           id: data.id,
--- query/index.js
+++ query/index.js
@@ -51,7 +51,7 @@ app.post("/events", (req, res) => {
 app.listen(port, async () => {
   console.log(`Server is running on port ${port}`);
 
-  const res = await axios.get("http://localhost:4005/events");
+  const res = await axios.get("http://event-bus-srv:4005/events");
   for (let event of res.data) {
     console.log("Processing event:", event.type);
     handleEvent(event.type, event.data);
